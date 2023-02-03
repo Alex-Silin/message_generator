0eb70c6e7851111eb70c6ccea64ebd6a5f61a685
AlexSilin <115801448+Alex-Silin@users.noreply.github.com>
2023-02-03 16:50:22 +0300

    working version

--- generator.js
+++ generator.js
@@ -1,3 +1,15 @@
 const greetings = ['Hello', 'Greetings', 'Great', 'Firework', 'I wish you good health', 'Chao', 'Hello', 'Shalom '];
 const wish = ['good', 'have a nice', 'great', 'wonderful',];
 const time = ['evenings', 'day', 'week', 'month', 'morning', 'year'];
+
+let oneArr;
+    oneArr = greetings[Math.floor(Math.random()*7)];
+
+let twoArr;
+    twoArr = wish[Math.floor(Math.random()*3)]
+
+let threeArr;
+    threeArr = time[Math.floor(Math.random()*5)]
+
+console.log(`${oneArr}, ${twoArr} ${threeArr}!`)
+
