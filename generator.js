6d6a1ad9da8f0a76e23462c8ddb9ca540340e9c7
AlexSilin <115801448+Alex-Silin@users.noreply.github.com>
2023-02-03 17:40:33 +0300

    Variables merged into one function

--- generator.js
+++ generator.js
@@ -2,14 +2,13 @@ const greetings = ['Hello', 'Greetings', 'Great', 'Firework', 'I wish you good h
 const wish = ['good', 'have a nice', 'great', 'wonderful',];
 const time = ['evenings', 'day', 'week', 'month', 'morning', 'year'];
 
-let oneArr;
-    oneArr = greetings[Math.floor(Math.random()*7)];
-
-let twoArr;
-    twoArr = wish[Math.floor(Math.random()*3)]
-
-let threeArr;
-    threeArr = time[Math.floor(Math.random()*5)]
-
-console.log(`${oneArr}, ${twoArr} ${threeArr}!`)
-
+const newArr = () => {
+    let newOneArr;
+    newOneArr = greetings[Math.floor(Math.random()*7)];
+let newTwoArr;
+    newTwoArr = wish[Math.floor(Math.random()*3)]
+let NewThreeArr;
+NewThreeArr = time[Math.floor(Math.random()*5)]
+    return `${newOneArr}, ${newTwoArr} ${NewThreeArr}!`
+   };
+   console.log(newArr());
