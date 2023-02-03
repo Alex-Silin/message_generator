4f224b0efda214a79df85de33f7142a633fd04d9
AlexSilin <115801448+Alex-Silin@users.noreply.github.com>
2023-02-03 16:45:50 +0300

    first commit

---
+++ generator.js
@@ -0,0 +1,3 @@
+const greetings = ['Hello', 'Greetings', 'Great', 'Firework', 'I wish you good health', 'Chao', 'Hello', 'Shalom '];
+const wish = ['good', 'have a nice', 'great', 'wonderful',];
+const time = ['evenings', 'day', 'week', 'month', 'morning', 'year'];
